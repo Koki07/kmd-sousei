dc3517cad62a025f838bc49dc72ef4701f3010d0
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-12-15 10:56:17 +0900

    delete panic.py

--- panic.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-import urllib.request
-
-u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
-data = u.read()
-f = open('rt22.xml', 'wb')
-f.write(data)
-f.close
-
